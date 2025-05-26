bcfb30027ae6fd1e38a70bdb8cec6724e542e787
Lucas Martins <lucas.mrt.dev@gmail.com>
2025-05-26 01:09:25 -0300

    feat: criado um componente de input consumindo o wrapper genérico

---
+++ finsight-frontend/src/components/input/input.tsx
@@ -0,0 +1,67 @@
+import { forwardRef, InputHTMLAttributes, ReactNode } from "react";
+import { FieldWrapper, FieldWrapperProps } from "./fieldWrapper";
+import { cn } from "@/lib/utils";
+
+type InputProps = Omit<FieldWrapperProps, "className" | "children"> &
+  InputHTMLAttributes<HTMLInputElement> & {
+    iconLeft?: ReactNode;
+    iconRight?: ReactNode;
+  };
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  (
+    {
+      className,
+      type,
+      label,
+      error,
+      description,
+      iconLeft,
+      iconRight,
+      id,
+      ...props
+    },
+    ref,
+  ) => {
+    return (
+      <FieldWrapper
+        htmlFor={id}
+        label={label}
+        error={error}
+        description={description}
+      >
+        <div className="relative">
+          {iconLeft && (
+            <span className="text-muted-foreground absolute top-1/2 left-3 -translate-y-1/2">
+              {iconLeft}
+            </span>
+          )}
+          <input
+            className={cn(
+              "border-input ring-offset-background flex h-10 w-full rounded-md border bg-transparent px-3 py-2 text-sm shadow-sm transition-colors",
+              "file:text-foreground file:border-0 file:bg-transparent file:text-sm file:font-medium",
+              "placeholder:text-muted-foreground",
+              "focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:outline-none",
+              "disabled:cursor-not-allowed disabled:opacity-50",
+              iconLeft && "pl-11",
+              iconRight && "pr-11",
+              className,
+            )}
+            type={type}
+            ref={ref}
+            {...props}
+          />
+          {iconRight && (
+            <span className="text-muted-foreground absolute top-1/2 right-3 -translate-y-1/2">
+              {iconRight}
+            </span>
+          )}
+        </div>
+      </FieldWrapper>
+    );
+  },
+);
+
+Input.displayName = "Input";
+
+export { Input };
