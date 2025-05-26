c946c6ede6c2b9a67241525c0f7b69356a408460
Lucas Martins <lucas.mrt.dev@gmail.com>
2025-05-26 01:08:38 -0300

    feat: criado um wrapper genérico para inputs

---
+++ finsight-frontend/src/components/input/fieldWrapper.tsx
@@ -0,0 +1,38 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+export type FieldWrapperProps = {
+  label?: string;
+  htmlFor?: string;
+  description?: string;
+  error?: string;
+  className?: string;
+  children: ReactNode;
+};
+
+export const FieldWrapper = ({
+  label,
+  htmlFor,
+  description,
+  error,
+  className,
+  children,
+}: FieldWrapperProps) => {
+  return (
+    <div className={cn("space-y-1", className)}>
+      {label && (
+        <label
+          htmlFor={htmlFor}
+          className="text-foreground text-sm font-medium"
+        >
+          {label}
+        </label>
+      )}
+      {children}
+      {description && !error && (
+        <p className="text-muted-foreground text-sm">{description}</p>
+      )}
+      {error && <p className="text-destructive text-sm">{error}</p>}
+    </div>
+  );
+};
