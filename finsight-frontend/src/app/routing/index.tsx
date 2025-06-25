b66916475b726e69d3dff63ff99d6bf3b81fd24a
Lucas Martins <lucas.mrt.dev@gmail.com>
2025-06-25 01:15:36 -0300

    feat: cria base para a navbar e layout principal

--- finsight-frontend/src/app/routing/index.tsx
+++ finsight-frontend/src/app/routing/index.tsx
@@ -1,6 +1,7 @@
-import { Route, Routes } from "react-router-dom";
-import { Login } from "@/features/login/login";
+import { MainLayout } from "@/components/layout/mainLayout";
 import { Home } from "@/features/home/home";
+import { Login } from "@/features/login/login";
+import { Route, Routes } from "react-router-dom";
 import { PATHS } from "./paths";
 
 export const AppRouter = () => {
@@ -8,7 +9,9 @@ export const AppRouter = () => {
     <Routes>
       <Route path={PATHS.login} element={<Login />} />
 
-      <Route path={PATHS.home} element={<Home />} />
+      <Route element={<MainLayout />}>
+        <Route path={PATHS.home} element={<Home />} />
+      </Route>
     </Routes>
   );
 };
---
+++ finsight-frontend/src/components/layout/navbar.tsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+  return (
+    <nav className="border-border flex w-full items-center justify-between border-b p-4">
+      <h2 className="text-primary text-xl font-bold">FinSight</h2>
+    </nav>
+  );
+};
